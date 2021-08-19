cdb81a0b81ef15beb43142a8ba7232e1b1358d23
arunavacucse <87722853+arunavacucse@users.noreply.github.com>
2021-08-19 12:30:29 +0530

    Add files via upload

---
+++ Answer 1 a/books.sql
@@ -0,0 +1,14 @@
+CREATE TABLE books (
+   bookid INTEGER PRIMARY KEY,
+   title TEXT,
+   author TEXT,
+   price REAL);
+
+
+INSERT INTO books VALUES (29954, "The Psychology of Money", "Morgan Housel", 126.30);
+
+
+INSERT INTO books VALUES (701032, "Atomic Habits", "James Clear", 478.00);
+
+
+SELECT * FROM books;
